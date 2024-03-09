0a1bbc0856225bfa6f8b35896ef27eebe015c82d
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-03-08 18:14:26 -0600

    wrapped inquirer question in a function to make it repeat

--- Develop/server.js
+++ Develop/server.js
@@ -1,50 +1,61 @@
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "mainQuestion",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees",
-        "Add Employee",
-        "Update Employee Role",
-        "View All Roles",
-        "Add Role",
-        "View All Departments",
-        "Add Department",
-        "Quit",
-      ],
-    },
-  ])
-  .then((response) => {
-    const choice = response.mainQuestion;
-    console.log(choice);
-    switch (choice) {
-      case "View All Employees":
-        console.log("You chose to view all employees.");
-        break;
-      case "Add Employees":
-        console.log("You chose to add an employee.");
-        break;
-      case "Update Employee Role":
-        console.log("You chose to update employee role.");
-        break;
-      case "View All Roles":
-        console.log("You chose to view all roles.");
-        break;
-      case "Add Role":
-        console.log("You chose to add a role.");
-        break;
-      case "View All Departments":
-        console.log("You chose to view all departments.");
-        break;
-      case "Add Department":
-        console.log("You chose to add a department.");
-        break;
-      case "Quit":
-        console.log("You chose to quit.");
-        break;
-    }
-  });
+function questionList() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "mainQuestion",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Quit",
+        ],
+      },
+    ])
+    .then((response) => {
+      const choice = response.mainQuestion;
+      console.log(choice);
+      switch (choice) {
+        case "View All Employees":
+          console.log("You chose to view all employees.");
+          questionList();
+          break;
+        case "Add Employee":
+          console.log("You chose to add an employee.");
+          questionList();
+          break;
+        case "Update Employee Role":
+          console.log("You chose to update employee role.");
+          questionList();
+          break;
+        case "View All Roles":
+          console.log("You chose to view all roles.");
+          questionList();
+          break;
+        case "Add Role":
+          console.log("You chose to add a role.");
+          questionList();
+          break;
+        case "View All Departments":
+          console.log("You chose to view all departments.");
+          questionList();
+          break;
+        case "Add Department":
+          console.log("You chose to add a department.");
+          questionList();
+          break;
+        case "Quit":
+          console.log("You chose to quit.");
+          break;
+      }
+    });
+}
+
+questionList();
