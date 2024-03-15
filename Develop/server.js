8f6c1989f3a5d5162f790d4b17d396994269b6aa
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-03-15 12:18:07 -0500

    structured function lists

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,13 @@
 const inquirer = require("inquirer");
-const db = require("./config/connection.js");
 const cTable = require("console.table");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localHost",
+  user: "root",
+  password: "Jasmine07!",
+  database: "company_db",
+});
 
 function questionList() {
   inquirer
@@ -17,7 +24,14 @@ function questionList() {
           "Add Role",
           "View All Departments",
           "Add Department",
-          "Quit",
+          "Update Employee Managers",
+          "View Employees by Manager",
+          "View Employees by Department",
+          "Delete Department",
+          "Delete Role",
+          "Delete Employee",
+          "View Total Budget",
+          "Quit\n\n",
         ],
       },
     ])
@@ -26,6 +40,7 @@ function questionList() {
       console.log(choice);
       switch (choice) {
         case "View All Employees":
+          console.log("You chose to view all Employees.");
           getEmployeeList().then(() => {
             questionList();
           });
@@ -42,7 +57,8 @@ function questionList() {
             questionList();
           });
           break;
-        case "View All Roles": //completed and working
+        case "View All Roles":
+          console.log("You chose to view all roles.");
           viewRoles().then(() => {
             questionList();
           });
@@ -53,13 +69,57 @@ function questionList() {
             questionList();
           });
           break;
-        case "View All Departments": //completed and working
+        case "View All Departments":
+          console.log("You chose to view all departments.");
           viewDepartments().then(() => {
             questionList();
           });
           break;
         case "Add Department":
+          console.log("You chose to add a department.");
           addDepartment().then(() => {
+            questionList();
+          });
+          break;
+        case "Update Employee Managers":
+          console.log("You chose to update employee manager.");
+          updateEmployeeManager().then(() => {
+            questionList();
+          });
+          break;
+        case "View Employees by Manager":
+          console.log("You chose to view employees by manager.");
+          viewEmployeeByManager().then(() => {
+            questionList();
+          });
+          break;
+        case "View Employees by Department":
+          console.log("You chose to view employees by department.");
+          viewEmployeeByDepartment().then(() => {
+            questionList();
+          });
+          break;
+        case "Delete Department":
+          console.log("You chose to delete a department.");
+          deleteDepartment().then(() => {
+            questionList();
+          });
+          break;
+        case "Delete Role":
+          console.log("You chose to delete a role.");
+          deleteRole().then(() => {
+            questionList();
+          });
+          break;
+        case "Delete Employee":
+          console.log("You chose to delete an employee.");
+          deleteEmployee().then(() => {
+            questionList();
+          });
+          break;
+        case "View Total Budget":
+          console.log("You chose to view the budget.");
+          viewBudget().then(() => {
             questionList();
           });
           break;
@@ -72,258 +132,43 @@ function questionList() {
     });
 }
 
-//when the user chooses to view a list of employees
-async function viewEmployees() {
-  const empList = `SELECT CONCAT(first_name, ' ', last_name) AS name, id FROM employee`;
-  const employee_list = await db.query(empList);
+//functions that will return tables of info and will not require user input
+function getEmployeeList() {}
 
-  const result = employee_list[0].map((employee) => ({
-    name: employee.name,
-    value: employee.id,
-  }));
-  return result;
-}
+function viewRoles() {}
 
-function viewDepartments() {
-  let department_list = `SELECT id AS dept, name AS department_name FROM department`;
-  return new Promise((resolve) => {
-    db.query(department_list, (err, result) => {
-      console.table("\n\n", result, "\n\n");
-      resolve(questionList);
-    });
-  });
-}
+function viewDepartments() {}
 
-function viewRoles() {
-  let role_list = `SELECT title, salary FROM role`;
-  return new Promise((resolve) => {
-    db.query(role_list, (err, result) => {
-      console.table("\n\n", result, "\n\n");
-      resolve(questionList);
-    });
-  });
-}
+//functions that will return tables of info based on certain criteria
 
-function addRole() {
-  return new Promise((resolve) => {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "title",
-          message: "What is the name of the new role? \n",
-        },
-        {
-          type: "input",
-          name: "salary",
-          message: "What is the salary of this position? \n",
-        },
-        {
-          type: "list",
-          name: "department",
-          message: "What department does this position belong to? \n",
-          choices: getDepartmentList(),
-        },
-        {
-          type: "list",
-          name: "is_manager",
-          message: "Is this position a manager? \n",
-          choices: ["yes", "no"],
-        },
-      ])
-      .then((response) => {
-        let dep;
-        let sql1 = `SELECT id AS id FROM department WHERE name = '${response.department}'`;
-        console.log(sql1);
-        db.query(sql1, (err, result) => {
-          console.log(result[0].id);
-          dep = result[0].id;
-          let yesManager;
-          if (response.is_manager === "yes") {
-            yesManager = 1;
-          } else {
-            yesManager = 0;
-          }
+function viewEmployeeByManager() {}
 
-          let sql = `INSERT INTO role (title, salary, department_id, is_manager) VALUES (?,?,?,?)`;
-          let params = [response.title, response.salary, dep, yesManager];
-          console.log(params);
-          db.query(sql, params, (err, result) => {
-            console.log(
-              `You have added ${response.title} to the database.\n\n`
-            );
-            resolve(questionList);
-          });
-        });
-      });
-  });
-}
+function viewEmployeeByDepartment() {}
 
-function getDepartmentList() {
-  //count the number of departments
-  let deptNum;
-  const deptList = `SELECT COUNT(*) AS total FROM department`;
-  db.query(deptList, (err, result) => {
-    deptNum = result[0].total;
-  });
-  //create an empty list
-  let deptListNames = [];
-  //use a loop to iterate through each department and add it to a list
+//functions that add new employees, roles, departments
 
-  db.query(`SELECT name AS dept FROM department`, (err, result) => {
-    for (let i = 0; i < deptNum; i++) {
-      deptListNames.push(result[i].dept);
-    }
-  });
-  return deptListNames;
-}
+function addEmployee() {}
 
-async function getRoleList() {
-  //count the number of departments
-  const roleList = `SELECT id, title, department_id, is_manager FROM role`;
-  const roleListQuery = await db.query(roleList);
+function addRole() {}
 
-  const roleListResults = roleListQuery[0].map((employee) => ({
-    id: employee.id,
-    name: employee.title,
-    dept_id: employee.department_id,
-    is_manager: employee.is_manager,
-  }));
-  return roleListResults;
-}
+function addDepartment() {}
 
-function addDepartment() {
-  return new Promise((resolve) => {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "title",
-          message: "What is the name of the new department? \n",
-        },
-      ])
+//functions that update information
 
-      .then((response) => {
-        let newDept = response.title;
-        console.log(newDept);
-        let sqldep = `INSERT INTO department (name) VALUES (?)`;
-        db.query(sqldep, [newDept], (err, result) => {
-          console.log(`You have added ${response.title} to the database.\n\n`);
-          resolve(questionList);
-        });
-      });
-  });
-}
+function updateRole() {}
 
-async function getManagerList() {
-  const manList = `SELECT CONCAT(first_name, ' ', last_name) AS name, role_id FROM employee LEFT JOIN role ON employee.role_id=role.id WHERE role.is_manager=1`;
+function updateEmployeeManager() {}
 
-  const manListNames = await db.query(manList);
+//functions that delete info
 
-  const manListResults = manListNames[0].map((employees) => ({
-    name: employees.name,
-    role_id: employees.role_id,
-  }));
-  return manListResults;
-}
+function deleteEmployee() {}
 
-function addEmployee() {
-  return new Promise(async (resolve) => {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "newFirstName",
-          message: "What is the employee's first name?",
-        },
-        {
-          type: "input",
-          name: "newLastName",
-          message: "What is the employee's last name?",
-        },
-        {
-          type: "list",
-          name: "newRole",
-          message: "What is the employee's role?",
-          choices: await getRoleList(),
-        },
-        {
-          type: "list",
-          name: "newSupervisor",
-          message: "Who is the employee's manager?",
-          choices: getManagerList(),
-        },
-      ])
-      .then((response) => {
-        const empRole = `SELECT id AS id FROM role WHERE title='${response.newRole}'`;
-        db.query(empRole, (err, result) => {
-          const newEmpRole = result[0].id;
-          const manName = `SELECT id AS id FROM employee WHERE CONCAT(first_name, ' ', last_name)='${response.newSupervisor}'`;
-          db.query(manName, (err, result) => {
-            const newManName = result[0].id;
-            const insertEmp = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)`;
-            const empParams = [
-              response.newFirstName,
-              response.newLastName,
-              newEmpRole,
-              newManName,
-            ];
-            db.query(insertEmp, empParams, (err, result) => {
-              console.log(
-                `You have added ${response.newFirstName} ${response.newLastName} to the database.\n\n`
-              );
-              resolve(questionList);
-            });
-          });
-        });
-      });
-  });
-}
+function deleteRole() {}
 
-async function getEmployeeList() {
-  const empNameQuery = `SELECT CONCAT(first_name, ' ', last_name) AS name, id FROM employee`;
-  const empListNames = await db.query(empNameQuery);
+function deleteDepartment() {}
 
-  const result = empListNames[0].map((employee) => ({
-    name: employee.name,
-    value: employee.id,
-  }));
-  return result;
-}
+//functions that calculate
 
-function updateRole() {
-  return new Promise(async (resolve, reject) => {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "employee",
-          message: "Select the employee name you would like to update.\n\n",
-          choices: await getEmployeeList(),
-        },
-        {
-          type: "list",
-          name: "newRole",
-          message: "What is the employee's new role?\n\n",
-          choices: await getRoleList(),
-        },
-        {
-          type: "list",
-          name: "newSupervisor",
-          message: "Who is the employee's manager?\n\n",
-          choices: await getManagerList(),
-        },
-      ])
-      .then((response) => {
-        console.log(response.employee);
-        resolve(questionList);
-      })
-      .catch((err) => {
-        console.log(err);
-        reject(err);
-      });
-  });
-}
+function viewBudget() {}
 
-//questionList();
-viewEmployees();
+questionList();
